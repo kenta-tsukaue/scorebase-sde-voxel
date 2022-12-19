d78b758b13482091b9e7eeab0a6fb97584a5bffd
kenta-tsukaue <110271795+kenta-tsukaue@users.noreply.github.com>
2022-12-20 08:34:00 +0900

    サンプリングの形を変更

--- run_lib.py
+++ run_lib.py
@@ -115,7 +115,7 @@ def train(config, workdir):
   # Building sampling functions 
   if config.training.snapshot_sampling:
     sampling_shape = (config.training.batch_size, config.data.num_channels,
-                      config.data.image_size, config.data.image_size)
+                      config.data.image_size, config.data.image_size, config.data.image_size)
     sampling_fn = sampling.get_sampling_fn(config, sde, sampling_shape, inverse_scaler, sampling_eps)
 
   num_train_steps = config.training.n_iters
