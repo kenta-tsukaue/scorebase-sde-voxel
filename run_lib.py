439bc1e8ff77d5a0e0526e01dcb9cab8501c5e3e
kenta-tsukaue <110271795+kenta-tsukaue@users.noreply.github.com>
2022-12-19 20:06:04 +0900

    テスト用のバッチの前処理のミスを修正

--- run_lib.py
+++ run_lib.py
@@ -156,7 +156,7 @@ def train(config, workdir):
       eval_batch = torch.from_numpy(next(eval_iter)['image']._numpy()).to(config.device).float()
       eval_batch = eval_batch.permute(0, 3, 1, 2)
       eval_batch = scaler(eval_batch)
-      batch = torch.unsqueeze(batch,dim=-4)  #  add channel axis for debug
+      eval_batch = torch.unsqueeze(eval_batch,dim=-4)  #  add channel axis for debug
       eval_loss = eval_step_fn(state, eval_batch)
       logging.info("step: %d, eval_loss: %.5e" % (step, eval_loss.item()))
 
