b01b34fa9ff6f9ba820dbe07f25de24dac41a5b2
kenta-tsukaue <110271795+kenta-tsukaue@users.noreply.github.com>
2022-12-19 16:28:43 +0900

    プリントを追加

--- losses.py
+++ losses.py
@@ -78,13 +78,14 @@ def get_sde_loss_fn(sde, train, reduce_mean=True, continuous=True, likelihood_we
     Returns:
       loss: A scalar that represents the average loss value across the mini-batch.
     """
-    #print(batch.shape) : (32, 10, 32, 32)
+    print("バッチの形は" + batch.shape) #: (32, 10, 32, 32)
     score_fn = mutils.get_score_fn(sde, model, train=train, continuous=continuous)
     t = torch.rand(batch.shape[0], device=batch.device) * (sde.T - eps) + eps
     z = torch.randn_like(batch) #zは加えるノイズのこと
 
     mean, std = sde.marginal_prob(batch, t)
     perturbed_data = mean + std[:, None, None, None] * z
+    print("データの形は" + perturbed_data.shape)
     score = score_fn(perturbed_data, t)#DDPMに渡すのはノイズが加えられたデータ
     #このscoreは結局何を出しているかと言うと、加えたノイズを出している
 
