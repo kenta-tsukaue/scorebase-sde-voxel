ed3008c3ecad245f5196b0097cb4d8543b955a9c
kenta-tsukaue <110271795+kenta-tsukaue@users.noreply.github.com>
2022-12-20 11:48:19 +0900

    mode.nfを32 => 8, サンプリング回数を50000=>5000

--- configs/subvp/slice_ddpm_continuous.py
+++ configs/subvp/slice_ddpm_continuous.py
@@ -45,7 +45,7 @@ def get_config():
   model.ema_rate = 0.9999
   model.normalization = 'GroupNorm'
   model.nonlinearity = 'swish'
-  model.nf = 32
+  model.nf = 8
   model.ch_mult = (1, 2, 2, 2)
   model.num_res_blocks = 2
   model.attn_resolutions = (16,)
