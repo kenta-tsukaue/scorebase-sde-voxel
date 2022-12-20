dd5817017aca83ec4343c7c171f05a1c2305eb76
kenta-tsukaue <110271795+kenta-tsukaue@users.noreply.github.com>
2022-12-20 09:21:42 +0900

    euler_maruyama内の掛け算による形の変更を克服

--- sampling.py
+++ sampling.py
@@ -183,7 +183,7 @@ class EulerMaruyamaPredictor(Predictor):
     z = torch.randn_like(x)
     drift, diffusion = self.rsde.sde(x, t)
     x_mean = x + drift * dt
-    x = x_mean + diffusion[:, None, None, None] * np.sqrt(-dt) * z
+    x = x_mean + diffusion[:, None, None, None, None] * np.sqrt(-dt) * z
     return x, x_mean
 
 
