eac6ea62e713733e40713c0ed35dd1ccc20ff8ab
kenta-tsukaue <110271795+kenta-tsukaue@users.noreply.github.com>
2023-01-11 15:01:11 +0900

    データを変更

--- configs/default_slice_configs.py
+++ configs/default_slice_configs.py
@@ -45,7 +45,7 @@ def get_default_configs():
   data.centered = False
   data.uniform_dequantization = False
   data.num_channels = 1
-  data.data_file = "data_set_channel_32_5"
+  data.data_file = "data_set_point10_channel"
 
   # model
   config.model = model = ml_collections.ConfigDict()
