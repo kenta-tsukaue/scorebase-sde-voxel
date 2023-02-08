5c921948b7f87a496dffe8e49a313a7844cb2fe1
kenta-tsukaue <110271795+kenta-tsukaue@users.noreply.github.com>
2023-02-08 15:37:23 +0900

    ファイルパスを変更

--- configs/default_slice_configs.py
+++ configs/default_slice_configs.py
@@ -45,7 +45,7 @@ def get_default_configs():
   data.centered = False
   data.uniform_dequantization = False
   data.num_channels = 3
-  data.data_file = "data_set_GPCR_1"
+  data.data_file = "data_set_gpcr_1"
 
   # model
   config.model = model = ml_collections.ConfigDict()
